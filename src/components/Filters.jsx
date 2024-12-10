1ec4fe48937d2bab79415be41c11ba85bc19772a
Mano9113 <manoranjanke39@gmail.com>
2024-12-10 13:26:56 +0530

    Update Filters.jsx

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,41 +1,22 @@
-import React, {useState} from 'react'
+import React from 'react';
 
-const Filters = ({onFilterChange}) => {
-  const [activeFilter, setFilter] = useState('All');
+const Filters = ({ activeFilter, onFilterChange }) => {
+  const filters = ['All', 'Favourite', 'Read', 'Unread'];
 
-  const handleFilterClick = (filter) => { 
-    setFilter(filter);
-    onFilterChange(filter);
-  }
   return (
-    <>  
-        <div className='d-flex align-items-center gap-2 m-3'>
-            <span>Filter By:</span>
-            <button className={`btn ${activeFilter === 'All' ? 'btn-outline-primary' : 'btn-secondary'}`} 
-                    onClick={() => handleFilterClick('All')}
-                    >All
-            </button>
-            <button
-                className={`btn ${activeFilter === 'Favourite' ? 'btn-outline-primary' : 'btn-secondary'}`}
-                onClick={() => handleFilterClick('Favourite')}
-              >
-                Favourite
-              </button>
-              <button
-                className={`btn ${activeFilter === 'Read' ? 'btn-outline-primary' : 'btn-secondary'}`}
-                onClick={() => handleFilterClick('Read')}
-              >
-                Read
-              </button>
-              <button
-                className={`btn ${activeFilter === 'Unread' ? 'btn-outline-primary' : 'btn-secondary'}`}
-                onClick={() => handleFilterClick('Unread')}
-              >
-                Unread
-              </button>
-        </div>
-    </>
-  )
-}
+    <div className="d-flex align-items-center gap-2 m-3">
+      <span>Filter By:</span>
+      {filters.map((filter) => (
+        <button
+          key={filter}
+          className={`btn ${activeFilter === filter ? 'btn-outline-primary' : 'btn-secondary'}`}
+          onClick={() => onFilterChange(filter)}
+        >
+          {filter}
+        </button>
+      ))}
+    </div>
+  );
+};
 
-export default Filters
+export default Filters;
